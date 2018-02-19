f3d58727b4c2528416718ac0ad694174539bca20
rzhade3 <rzhade@gatech.edu>
2018-02-19 14:39:30 -0500

    Screw CSS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var companies = ["Tinder", "Snapchat", "Uber", "Lyft", "Google", "Twitter", "Facebook", "AirBnB", "Apple", "Github", "Amazon", "YouTube", "eBay", "Venmo", "Netflix", "Wikipedia", "Spotify", "Twitch", "Reddit"];
-var buzzwords = ["machine learning", "blockchain", "social media", "artificial intelligence", "robotics", "Internet of Things", "music technology", "cloud computing", "big data", "deep learning", "data mining", "virtual reality", "cryptocurrency"];
+var buzzwords = ["machine learning", "blockchain", "social media", "AI", "robotics", "Internet of Things", "music technology", "cloud computing", "big data", "deep learning", "data mining", "virtual reality", "cryptocurrency"];
 
 $("body").click(function() {
 	randomize();
@@ -8,8 +8,8 @@ $("body").click(function() {
 function randomize() {
 	var comp = companies[Math.floor(Math.random() * companies.length)];
 	var buzz = buzzwords[Math.floor(Math.random() * buzzwords.length)];
-	var string = "It's like " + comp + " but for " + buzz + ".";
-	$("h1").text(string);
+	var string = "It's like <br><u>" + comp + "</u>,<br> but for <br><u>" + buzz + "</u>";
+	$("h1").html(string);
 }
 
 randomize();
